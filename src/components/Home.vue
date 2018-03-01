11012d441c315dfe55abc50a9e5429c3c60a58a4
Unknown <and.epost@gmail.com>
2018-03-01 12:57:05 +0100

    Add home welcometext

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,29 @@
 <template>
   <div id="home">
     <h1>Projects and ideas</h1>
+    <p>
+      {{ home.welcomeText }}
+    </p>
   </div>
 </template>
 
 <script>
+import { db } from './firebase';
+
 export default {
   name: 'Home',
+  data() {
+    return {
+      home: {},
+    };
+  },
+
+  firebase: {
+    home: {
+      source: db.ref('home'),
+      asObject: true,
+    },
+  },
 };
 </script>
 
@@ -15,5 +32,12 @@ export default {
   padding: 8px;
   background-color: #d5d5d5;
   height: 95vh;
+}
+
+#home p {
+  line-height: 2em;
+  max-width: 600px;
+  margin-left: 5%;
+  margin-right: 5%;
 }
 </style>
